d5e956b89dff41de97aee025846d76c9a003ba06
Marcel R <github.riga@icloud.com>
2019-12-02 20:25:09 +0100

    Adapt lbn tutotial to latest lbn version.

--- docker/tf1/run.sh
+++ docker/tf1/run.sh
@@ -2,11 +2,7 @@
 
 action() {
     # determine the directy of this file (/docker) and the repo dir
-    if [ ! -z "$ZSH_VERSION" ]; then
-        local this_file="${(%):-%x}"
-    else
-        local this_file="${BASH_SOURCE[0]}"
-    fi
+    local this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
     local this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
     local repo_dir="$( cd "$( dirname "$( dirname "$this_dir" )" )" && pwd )"
 
